912575a861d952ce2a2ad73c3979838627eb9c3a
Qazi Fabia Hoq <52504271+qazifabiahoq@users.noreply.github.com>
2024-03-01 01:02:41 -0500

    commit

--- app.py
+++ app.py
@@ -1,35 +1,15 @@
 from flask import Flask, render_template
-import plotly.graph_objects as go
-import pandas as pd
-import numpy as np
-from ForestFireVSCode import your_function_or_class  # Import your functions or classes from ForestFireVSCode.py
+from ForestFireVSCode import process_data, generate_plots
 
-@app.route('/', methods=['GET', 'POST'])
+app = Flask(__name__)
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        selected_year = request.form['yearDropdown']
-        # Use selected_year to update your plots
+    # Process data and generate plots
+    data = process_data()
+    plot_html = generate_plots(data)
 
-        # Update the bar chart
-        new_bar_data = {
-            'x': [1960, 1970, 1980, 1990, 2000, 2010, 2020],
-            'y': [100, 150, 130, 170, 200, 180, 190],
-            'type': 'bar',
-            'marker': {'color': 'blue'},
-            'opacity': 0.5
-        }
-        bar_plot = go.Figure(data=[new_bar_data])
-        bar_plot_html = bar_plot.to_html(include_plotlyjs='cdn')
+    return render_template('index.html', plot=plot_html)
 
-        # Update other plots similarly
-
-        return render_template('index.html', bar_plot=bar_plot_html, ...)
-    else:
-        # Use your functions or classes from ForestFireVSCode.py to process data
-        data = your_function_or_class.process_data()
-
-        # Use processed data to create plots
-        fig = go.Figure(...)
-        plot_html = fig.to_html(include_plotlyjs='cdn')
-
-        return render_template('index.html', plot=plot_html)
+if __name__ == '__main__':
+    app.run(debug=True)
