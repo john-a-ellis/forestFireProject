af0e4b41624a7e898ce0c9e166b9652a38fbbf7e
john-a-ellis <145600714+john-a-ellis@users.noreply.github.com>
2024-03-08 13:37:27 -0500

    pushing updates for gitpages

--- aj_app.py
+++ aj_app.py
@@ -5,10 +5,7 @@ import datetime
 from flask import Flask, render_template, jsonify, send_file, request, url_for
 
 app = Flask(__name__)
-"""
-Create a new route to generate a weather/fire animation.
-Accepts a start and end date in the form YYYY-MM-DD.
-"""
+
 @app.get("/")
 def index():
     return render_template('aj_index.html')
@@ -37,7 +34,6 @@ def get_weather_animation():
     make_weather_and_fire_animation(start_date, end_date)
     myMap = genMap(myYear, myMonth)
     myMap.save('static/FireWithWater.html')
-    # render_template("FireWithWater.html")
     send_file("static/animation.gif", mimetype="image/animation")
     return render_template('aj_index.html')
 
